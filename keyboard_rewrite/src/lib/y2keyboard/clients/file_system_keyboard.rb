a6bec9be8cc52c1961f29187920891230ea75695
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2019-01-07 20:32:44 +0000

    add client

---
+++ keyboard_rewrite/src/lib/y2keyboard/clients/file_system_keyboard.rb
@@ -0,0 +1,38 @@
+# Copyright (c) [2018] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../dialogs/layout_selector"
+require_relative "../strategies/file_system_strategy"
+require_relative "../keyboard_layout"
+require "yaml"
+
+module Y2Keyboard
+  module Clients
+    # Client with systemd implementation.
+    class FileSystemKeyboard
+      def self.run
+        path = File.join(__dir__, "../data/keyboards.yml")
+        layout_definitions = YAML.load_file(path)
+        systemd_strategy = Y2Keyboard::Strategies::FileSystemStrategy.new
+        Y2Keyboard::KeyboardLayout.use(systemd_strategy, layout_definitions)
+        Y2Keyboard::Dialogs::LayoutSelector.new.run
+      end
+    end
+  end
+end
