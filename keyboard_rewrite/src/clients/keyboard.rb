7aa2b32767e0de783f5a43afd2d4a98fcd9452f2
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2019-01-07 20:33:01 +0000

    use file system client

--- keyboard_rewrite/src/clients/keyboard.rb
+++ keyboard_rewrite/src/clients/keyboard.rb
@@ -1,3 +1,4 @@
 require_relative "../lib/y2keyboard/clients/systemd_keyboard"
+require_relative "../lib/y2keyboard/clients/file_system_keyboard"
 
-Y2Keyboard::Clients::SystemdKeyboard.run
+Y2Keyboard::Clients::FileSystemKeyboard.run
